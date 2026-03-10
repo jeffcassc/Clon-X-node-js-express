d2b811f63d457899fd159096da449d108b853400
yefrey <yefreycassiani16@gmail.com>
2026-03-10 10:49:57 -0500

    creacion del register

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,3 +8,45 @@ const generateToken = (id)=>{
         {expiresIn: '10d'}
     )
 }
+
+
+const register = async (req,res)=>{
+    try {
+        const { username, email, password } = req.body
+
+        if (!username || !email || !password){
+            return res.status(400).json({
+                message: 'todo los campos son requeridos'
+            })
+        }
+        const userExist = await User.findOne({
+            $or:[{username}, {email}]
+        }) 
+
+        if(userExist){
+            return res.status(400).json({
+                message: 'el usuario o el email ya esta registrado'
+            })
+        }
+
+        const user = await User.create({
+            username,
+            email,
+            password,
+            displayName: username
+        })
+
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            displayName: user.displayName,
+            avatar: user.avatar,
+            bio: user.bio,
+            token: generateToken(user._id)
+        })
+    } catch (error) {
+        console.error('Error en registro:', error);
+        res.status(500).json({message: 'Error en el servidor'});
+    }
+}
