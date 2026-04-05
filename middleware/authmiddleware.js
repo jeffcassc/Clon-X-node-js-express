b82d73174f09a674bdcf7be4fa0cec7178c9c7ad
yefrey <yefreycassiani16@gmail.com>
2026-04-05 18:20:45 -0500

    actualiacion del middleware

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,29 +1,30 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js'
 
-const protect = async (req,res)=>{
+const protect = async (req,res,next)=>{
     let token
 
-    if(req.headers.authorization && req.headers.authorization.startWith('Bearer')){
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         try {
             token = req.headers.authorization.split(' ')[1]
 
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            req.user = await User.findById(decoded).select('-password')
+            req.user = await User.findById(decoded.id).select('-password')
 
             if(!req.user){
                 return res.status(401).json({
                     message:'Usuario no encontrado'
                 })
             }
-            next
+            next()
         } catch (error) {
             console.error('Error en autenticación:', error.message);
             res.status(401).json({ message: 'Token no válido' });
         }
-        if (!token) {
-        res.status(401).json({ message: 'No autorizado, token no encontrado' });
+        
     }
+    if (!token) {
+        res.status(401).json({ message: 'No autorizado, token no encontrado' });
     }
 }
 
