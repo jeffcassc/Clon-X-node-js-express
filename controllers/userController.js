7f2ee851076275dc625f57022da1905701dcb800
yefrey <yefreycassiani16@gmail.com>
2026-04-08 21:48:46 -0500

    feat(User): actualizacion de usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,5 +29,42 @@ const getUserProfile = async (req,res)=>{
         res.status(500).json({ message: 'Error en el servidor' })
     }
 }
+const updateProfile = async (req,res)=>{
+    try {
+        const { displayName, bio, avatar, coverImage } = req.body
 
-export {getUserProfile}
+        const user = await User.findById(req.user._id)
+        if(!user){
+            return res.status(400).json({
+                message: 'Usuario no encontrado'
+            })
+        }
+
+        if(displayName) user.displayName = displayName
+        if(bio !== undefined) user.bio = bio
+        if(avatar) user.avatar = avatar
+        if(coverImage) user.coverImage = coverImage
+
+        await user.save()
+
+        res.json({
+            _id: user._id,
+            username: user.username,
+            displayName: user.displayName,
+            avatar: user.avatar,
+            bio: user.bio,
+            coverImage: user.coverImage,
+        })
+
+    } catch (error) {
+        console.error('Error actualizando usuario:', error)
+        res.status(500).json({ message: 'Error en el servidor' })
+    }
+}
+const followUser = async ()=>{}
+const unFollowUser = async ()=>{}
+const searchUsers = async ()=>{}
+const getSuggestedUsers = async ()=>{
+}
+
+export {getUserProfile, updateProfile }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getUserProfile } from '../controllers/userController.js'
+import { getUserProfile, updateProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authmiddleware.js'
 
 const router = express.Router()
 
 router.post('/:username', getUserProfile)
+router.put('/profile',protect, updateProfile)
 
 
 
