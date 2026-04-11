44d443030d4cade20f075ddd784eb91d4350c002
yefrey <yefreycassiani16@gmail.com>
2026-04-11 15:21:46 -0500

    feat(models): modelo de un tweet

--- models/Tweet.js
+++ models/Tweet.js
@@ -1,11 +1,46 @@
 import mongoose from "mongoose";
 
 const tweetSchema = new mongoose.Schema({
-    content:{},
-    author:{},
-    likes:{},
-    comments:{},
-    image:{}
+    content:{
+        type: String,
+        required: [true, 'El contenido es requerido'],
+        maxlength: [280, 'El tweet no puede alcanzar los 280 caracteres']
+    },
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    likes:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments:[{
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        content:{
+            type: String,
+            required: [true, 'El contenido es requerido'],
+            maxlength: [280, 'El tweet no puede alcanzar los 280 caracteres']
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now
+        }
+    }],
+    image:{
+        type: String,
+        default: ''
+    }
 },{
     timestamps: true
 })
+
+tweetSchema.index({ createdAt: -1 })
+
+const tweet = mongoose.model('tweet', tweetSchema)
+
+export default tweet
